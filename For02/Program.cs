f73fd167e9c9f8072dafdaaafa34c83361ff648f
Daniela Marques <116357067+daniela2785@users.noreply.github.com>
2023-02-09 17:57:41 -0300

    Update Program.cs

--- For02/Program.cs
+++ For02/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Globalization;
+
+
+namespace ExerciciosPropostos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //02
+            /*int N = int.Parse(Console.ReadLine());
+
+            int[] vet = new int[N];
+
+            string[] s = Console.ReadLine().Split(' ');
+
+            for(int i = 0; i < N; i++){
+                vet[i] = int.Parse(s[i]);
+            }
+
+            for (int i = 0; i < N; i++){
+                if (vet[i] % 2 == 0){
+                    Console.Write(vet[i] + " ");
+                }
+            }
+            Console.WriteLine();
+
+            int quantidade = 0;
+            for (int i = 0; i < N; i++){
+                if (vet[i] % 2 == 0) {
+                    quantidade++;
+                }
+            }
+
+            Console.Write(quantidade);*/
+
+        }
+    }
+}
